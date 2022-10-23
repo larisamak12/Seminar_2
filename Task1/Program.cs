4dbe37db8a155aadfcdc7861d87b55190c6d0d8b
Larisa <larisamak12@mail.ru>
2022-10-23 16:41:56 +0300

    Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая принимает на вход трёхзначное число 
+//  и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число ");
+int num=Convert.ToInt32(Console.ReadLine());
+
+int num1=num/100;
+int num2=num/10;
+int num3=num%10;
+
+Console.WriteLine($"Последняя цифра числа {num} равна {num3}");
